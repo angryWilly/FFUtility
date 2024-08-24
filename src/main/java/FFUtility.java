327a000f4f9479e58571ba579b73133db0304c11
angrywilly <kirill.teyxrib@yandex.ru>
2024-08-24 18:45:29 +0700

    started infrastructure

---
+++ src/main/java/FFUtility.java
@@ -0,0 +1,5 @@
+public class FFUtility {
+    public FFUtility(String[] args) {
+
+    }
+}
--- src/main/java/org/angryWilly/FFUtility.java
+++
@@ -1,6 +0,0 @@
-package org.angryWilly;
-
-public class FFUtility {
-    public static void main(String[] args) {
-    }
-}
